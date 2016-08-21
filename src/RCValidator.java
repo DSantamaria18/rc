d2488d2fa973954a5c93cc19e61e698f3fe1d1f6
David Santamaría Lago <dsantamarialago@ticketbis.com>
2016-08-21 12:45:40 +0200

    Replaced site list with combo

--- src/RCValidator.java
+++ src/RCValidator.java
@@ -13,61 +13,12 @@ public class RCValidator {
     private JCheckBox chkPUM;
     private JCheckBox chkLar;
     private JCheckBox chkAttendants;
-    private JLabel lblSiteList;
-    private JTextField textField1;
     private JCheckBox checkBox1;
-    private JCheckBox checkBox2;
-    private JCheckBox checkBox3;
-    private JCheckBox checkBox4;
-    private JCheckBox checkBox5;
-    private JCheckBox checkBox6;
-    private JCheckBox checkBox7;
-    private JCheckBox checkBox8;
-    private JCheckBox checkBox9;
-    private JCheckBox checkBox10;
     private JTextField textField2;
     private JButton BUSCARButton;
     private JPanel RCPanel;
-    private JCheckBox checkBox11;
-    private JCheckBox checkBox12;
-    private JCheckBox checkBox13;
-    private JCheckBox checkBox14;
-    private JCheckBox checkBox15;
-    private JCheckBox checkBox16;
-    private JCheckBox checkBox17;
-    private JCheckBox checkBox18;
-    private JCheckBox checkBox19;
-    private JCheckBox checkBox20;
-    private JCheckBox checkBox21;
-    private JCheckBox checkBox22;
-    private JCheckBox checkBox23;
-    private JCheckBox checkBox24;
-    private JCheckBox checkBox25;
-    private JCheckBox checkBox26;
-    private JCheckBox checkBox27;
-    private JCheckBox checkBox28;
-    private JCheckBox checkBox29;
-    private JCheckBox checkBox30;
-    private JCheckBox checkBox31;
-    private JCheckBox checkBox32;
-    private JCheckBox checkBox33;
-    private JCheckBox checkBox34;
-    private JCheckBox checkBox35;
-    private JCheckBox checkBox36;
-    private JCheckBox checkBox37;
-    private JCheckBox checkBox38;
-    private JCheckBox checkBox39;
-    private JCheckBox checkBox40;
-    private JCheckBox checkBox41;
-    private JCheckBox checkBox42;
-    private JCheckBox checkBox43;
-    private JCheckBox checkBox44;
-    private JCheckBox checkBox45;
-    private JCheckBox checkBox46;
-    private JCheckBox checkBox47;
-    private JCheckBox checkBox48;
-    private JCheckBox checkBox49;
-    private JCheckBox checkBox50;
+    private JLabel lblSites;
+    private JComboBox comboBox1;
 
     public RCValidator() {
         BUSCARButton.addMouseListener(new MouseAdapter() {
@@ -92,6 +43,7 @@ public class RCValidator {
 
                 // SITES
 
+                if (checkBox1.isSelected())
 
 
                 RC rc = new RC();
